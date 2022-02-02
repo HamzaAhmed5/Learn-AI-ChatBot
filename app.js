a5024e979d15a4b41b28c4ff98d6c510ee072400
Hamza Ahmed <63982731+HamzaAhmed5@users.noreply.github.com>
2022-02-02 23:49:16 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+
+/*var num_1 = 10;
+var num_2 = 20;
+var result1 = num_1 + num_2;
+var result2 = num_1 - num_2;
+var result3 = num_1 * num_2;
+var result4 = num_1 / num_2;
+var result5 = num_1 % num_2;
+var result6 = num_1++;
+var result7 = ++num_1;
+
+document.write(result1 + "</br>")
+document.write(result2 + "</br>")
+document.write(result3 + "</br>")
+document.write(result4 + "</br>")
+document.write(result5 + "</br>")
+document.write(result6 + "</br>")
+document.write(result7 + "</br>")*/
+
+//var firstNumber = prompt("enter first number: ");
+//var secondNumber = prompt("enter second number: ");
+
+//var result = firstNumber + secondNumber;
+
+//document.write(result)
+
+//var name = prompt("Enter your name: ");
+//var result = "Hello, " + name +"!";
+//alert(result);
+
